e5b11cc91a7bbc33d259a905d6c0667dad452677
jharpin <154296932+jharpin@users.noreply.github.com>
2025-08-23 22:42:27 -0500

    documentacion

--- ejercicio3/Main.exs
+++ ejercicio3/Main.exs
@@ -1,4 +1,17 @@
 defmodule Main do
+  @moduledoc """
+  Módulo principal para calcular el salario neto de un empleado en función de las horas trabajadas
+  y el valor por hora. También maneja horas extras si el empleado supera las 160 horas trabajadas.
+  """
+  @doc """
+  Función principal que solicita los datos del empleado (nombre, horas trabajadas, valor por hora),
+  calcula el salario neto y muestra el resultado.
+  - Solicita el nombre del empleado.
+  - Solicita las horas trabajadas y las convierte a entero.
+  - Solicita el valor por hora y lo convierte a flotante.
+  - Calcula el salario neto considerando horas extras si es necesario.
+  - Muestra el salario neto del empleado.
+  """
   def main() do
     nombre = Util.input_data("Ingrese el nombre del empleado: ")
     horas_trabajadas = Util.input_data("Ingrese las horas trabajadas: ")
@@ -13,6 +26,10 @@ defmodule Main do
     |> Util.mostrar_mensaje()
   end
 
+  @doc """
+  Calcula el salario neto del empleado, considerando horas extras (por encima de 160 horas)
+  Si el empleado trabajó más de 160 horas, las horas extras se pagan al 125% del valor por hora.
+  """
   defp calcular_salario_neto({horas, valor_hora}) do
     if horas > 160 do
       horas_normales = 160
--- ejercicio3/Salario.java
+++ ejercicio3/Salario.java
@@ -1,17 +1,26 @@
 import javax.swing.JOptionPane;
 
 public class Salario {
-     public static void  main(String[] args) {
 
-    if(args.length > 0 && args[0].equals("input")) {
-        String mensaje = args[1];
-        String input = JOptionPane.showInputDialog(null, mensaje);
+    /**
+     * Función principal que maneja la visualización de mensajes o la solicitud de datos.
+    
+     * Si se pasa el argumento "input", muestra un cuadro de entrada para que el usuario ingrese un dato,
+     * el cual luego se imprime en la consola.
+     * Si se pasa solo un mensaje, se muestra en un cuadro de diálogo.
+     * Si no se pasa ningún argumento, se muestra un mensaje predeterminado.
+     */
+    public static void main(String[] args) {
 
-        System.out.println(input);
-    } else if (args.length > 0) {
-        JOptionPane.showMessageDialog(null, args[0]);
-    }else {
-        JOptionPane.showMessageDialog(null, "Mensaje no recibido");
+        if (args.length > 0 && args[0].equals("input")) {
+            String mensaje = args[1];
+            String input = JOptionPane.showInputDialog(null, mensaje);
+            System.out.println(input);
+        } else if (args.length > 0) {
+            JOptionPane.showMessageDialog(null, args[0]);
+        } else {
+            JOptionPane.showMessageDialog(null, "Mensaje no recibido");
+        }
     }
-   }
 }
+
